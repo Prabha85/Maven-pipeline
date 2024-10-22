b9da149088d2b5fdb48584d2091e24cbeb3a7d32
Prabha85 <159604698+Prabha85@users.noreply.github.com>
2024-10-22 20:26:36 +0530

    Create pipeline.java

---
+++ .settings/pipeline.java
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying...'
+                // Add your deployment steps here
+            }
+        }
+    }
+
+    post {
+        always {
+            junit '**/target/surefire-reports/*.xml' // Publish test results
+        }
+    }
+}
